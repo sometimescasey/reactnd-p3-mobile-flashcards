7a1af3b5e8879f9ca22544c40f87f3e735a83ac7
Casey Li <sometimescasey@gmail.com>
2020-06-16 10:21:43 -0400

    add colors and icons back

--- navigator/Navigator.js
+++ navigator/Navigator.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { Platform } from 'react-native';
-import { connect } from 'react-redux';
 import DeckScreen from '../screens/DeckScreen';
 import NewDeckScreen from '../screens/NewDeckScreen';
-
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { blue, cyan } from '../utils/colors';
 
 const Navigator = createBottomTabNavigator(
     routes = {
-        "Main": DeckScreen,
-        "New Deck": NewDeckScreen,
+        "Main": {
+            screen: DeckScreen,
+            navigationOptions: {
+                tabBarLabel: 'History',
+                tabBarIcon: ({ tintColor }) => (<FontAwesome
+                    name="home"
+                    size={30}
+                    color={tintColor}/>)
+            }},
+        "Add Entry": {
+                screen: NewDeckScreen,
+                navigationOptions: {
+                    tabBarLabel: 'Add Entry',
+                    tabBarIcon: ({ tintColor }) => (
+                        <FontAwesome
+                            name="dashboard"
+                            size={30}
+                            color={ tintColor }/>
+                    )
+                }
+            }, 
     },
-    config = {}
+    config={
+        navigationOptions: {
+            header: null,
+        },
+        tabBarOptions: {
+            inactiveTintColor: '#bbb',
+            activeTintColor: Platform.OS === 'ios' ? blue : 'white',
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === 'ios' ? 'white' : blue,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+            }
+        }
+
+    }
 );
 
 
